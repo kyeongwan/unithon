991997b5543b0bb606a697aaf27a5d328de14643
kyeongwan <lk@lemonlab.co.kr>
2016-02-14 00:20:17 +0900

    로그인버튼

--- app/src/main/java/unithon/here/MainActivity.java
+++ app/src/main/java/unithon/here/MainActivity.java
@@ -1,15 +1,50 @@
 package unithon.here;
 
+import android.app.Activity;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.nhn.android.naverlogin.OAuthLogin;
+import com.nhn.android.naverlogin.OAuthLoginDefine;
+import com.nhn.android.naverlogin.OAuthLoginHandler;
+import com.nhn.android.naverlogin.ui.view.OAuthLoginButton;
+//import com.nhn.android.oauth.R;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "OAuthSampleActivity";
+
+    /**
+     * client 정보를 넣어준다.
+     */
+    private static String OAUTH_CLIENT_ID = "jyvqXeaVOVmV";
+    private static String OAUTH_CLIENT_SECRET = "527300A0_COq1_XV33cf";
+    private static String OAUTH_CLIENT_NAME = "네이버 아이디로 로그인";
+
+
+    private static OAuthLogin mOAuthLoginInstance;
+    private static Context mContext;
+
+    /** UI 요소들 */
+    private TextView mApiResultText;
+    private static TextView mOauthAT;
+    private static TextView mOauthRT;
+    private static TextView mOauthExpires;
+    private static TextView mOauthTokenType;
+    private static TextView mOAuthState;
+
+    private OAuthLoginButton mOAuthLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
